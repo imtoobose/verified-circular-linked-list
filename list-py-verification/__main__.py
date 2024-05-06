adf442c8b04c18eaf96656918c9783a3df85d35b
Goku <mgokulkumar99@gmail.com>
2024-05-06 11:48:59 -0400

    Added Randomize and defined delete op in it

--- list-py-verification/__main__.py
+++ list-py-verification/__main__.py
@@ -1,5 +1,6 @@
 # Dafny program list.dfy compiled into Python
 import sys
+import random
 from typing import Callable, Any, TypeVar, NamedTuple
 from math import floor
 from itertools import count
@@ -54,7 +55,55 @@ def listAddTest(newNode: Node, head: Node):
     default__.list__add(newNode, head)
     print("Printing Linked List after list__add:")
     printList(head)
-    
+
+def delNodeEntryTest(entry: Node):
+    print("Printing Linked List before list__del__entry:")
+    printList(entry)
+    temp = entry.next
+    default__.list__del__entry(entry)
+    print("Printing Linked List after list__del__entry:")
+    printList(temp)
+
+def randomCheck():
+    nodeMap.clear()
+    global nodeCtr
+    nodeCtr = 0
+    node0 = createNewNode()
+    node1 = createNewNode()
+    node2 = createNewNode()
+    default__.list__add__tail(node1, node0)
+    default__.list__add__tail(node2, node0)
+    printList(node0)
+
+    for i in range(10):
+        listSize = getSize(node0)
+        if listSize == 1:
+             randomOp = random.random(1, 6)
+        else:
+            randomOp = random.random(0, 6)
+        
+        if randomOp == 0:   # Delete Operation
+            randomNode = random.randrange(1, listSize)
+            temp = node0
+            for hops in range(randomNode):
+                temp = temp.next
+            print("Printing List before deleting node in position" + str(randomNode))
+            printList(node0)
+            default__.list__del__entry(temp)
+            print("Printing List before deleting node")
+            printList(node0)
+        
+        if randomOp == 1:   # Delete Operation
+            randomNode = random.randrange(1, listSize)
+            temp = node0
+            for hops in range(randomNode):
+                temp = temp.next
+            print("Printing List before deleting node in position" + str(randomNode))
+            printList(node0)
+            default__.list__del__entry(temp)
+            print("Printing List before deleting node")
+            printList(node0)
+
 
 
 dll = createNewNode()
@@ -71,4 +120,5 @@ printList(dll)
 singletonTest(dll)
 listAddTest(new_dll2, dll)
 listAddTailTest(new_dll3, dll)
-
+randomCheck()
+print(nodeMap)
